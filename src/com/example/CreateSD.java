842019952a86fbe0579bb9e0e468e8b2ca007358
Anders Rundgren <anders.rundgren.net@gmail.com>
2025-09-01 09:10:40 +0200

    temp

--- src/com/example/CreateSD.java
+++ src/com/example/CreateSD.java
@@ -2,27 +2,101 @@ package com.example;
 
 import java.io.File;
 
+import java.security.KeyPair;
+
 import org.webpki.cbor.*;
+import org.webpki.crypto.AsymSignatureAlgorithms;
+import org.webpki.crypto.HashAlgorithms;
+import org.webpki.util.Base64URL;
+import org.webpki.util.HexaDecimal;
 import org.webpki.util.IO;
 import org.webpki.util.UTF8;
 
 public class CreateSD {
+
+    static final String OBJECT_ID = "https://example.com/sd-cbor";
+
     static CBORMap requestMap;
+    static StringBuilder template = new StringBuilder();
     static String requestDN;
 
-    static CBORMap result = new CBORMap();
+    static String baseDirectory;
 
-    static CBORMap readCBOR(String[] argc, String localName) {
-        return CBORDiagnosticNotation.convert(readDNText(argc, localName)).getMap();
+    static KeyPair issuerPair;
+
+    static CBORMap result = new CBORMap();
+    
+    
+    static void writeString(String fileName, String data) {
+        IO.writeFile(fileName, UTF8.encode(data));
+    }
+    
+    static String htmlIze(String string) {
+        return string.replace("&", "&amp;")
+                     .replace("<", "&lt;")
+                     .replace(">", "&gt;")
+                     .replace(" ", "&nbsp;")
+                     .replace("\n", "<br>");
     }
 
-    static String readDNText(String[] argc, String localName) {
-        return UTF8.decode(IO.readFile(argc[0] + File.separator + localName));
+    static CBORMap readCBOR(String localName) {
+        return CBORDiagnosticNotation.convert(readString(localName)).getMap();
+    }
+
+    static String readString(String localName) {
+        return UTF8.decode(IO.readFile(baseDirectory + File.separator + localName));
+    }
+
+    static String createBox(String raw) {
+        return "<div class='webpkihexbox' style='display:block'>" +
+               htmlIze(raw) +
+               "</div>";
+    }
+
+    static void replace(String holder, String text) {
+        int index = template.indexOf("@" + holder + "@");
+        if (index <= 0) {
+            throw new RuntimeException("no find: " + holder);
+        }
+        template.replace(index, index + holder.length() + 2, text);
     }
 
     public static void main(String[] argc) {
-        requestDN = readDNText(argc, "request.dn");
+        System.out.println(
+   HexaDecimal.encode(HashAlgorithms.SHA256.digest(
+            HexaDecimal.decode(
+                "82507AF7084B50BADEB57D49EA34627C7A521A601DB950"
+          //      "8350bae611067bb823486797da1ebbb52f836b414243442d3132333435361901f5"
+                
+                ))));
+ 
+        String sdjwt = Base64URL.encode(UTF8.encode("[\"lklxF5jMYlGTPUovMNIvCA\", \"US\"]"));
+        System.out.println(sdjwt);
+        System.out.println(
+   Base64URL.encode(HashAlgorithms.SHA256.digest(
+            UTF8.encode(sdjwt))));
+
+        sdjwt = Base64URL.encode(UTF8.encode("[\"eluV5Og3gSNII8EYnsxA_A\", \"family_name\", \"Doe\"]"));
+        System.out.println(sdjwt);
+        System.out.println(
+   Base64URL.encode(HashAlgorithms.SHA256.digest(
+            UTF8.encode(sdjwt))));
+
+
+
+        baseDirectory = argc[0];
+        template.append(readString("template.html"));
+        requestDN = readString("request.dn");
+        replace("input", createBox(requestDN));
+        String ikey = readString("ikey.dn");
+        CBORMap temp = CBORDiagnosticNotation.convert(ikey).getMap();
+        CBORObject kid = temp.remove(new CBORInt(2));
+        int alg = temp.remove(new CBORInt(3)).getInt32();
+        issuerPair = CBORKeyPair.convert(temp);
+        replace("ikey", createBox(ikey));
         requestMap = CBORDiagnosticNotation.convert(requestDN).getMap();
+        requestMap.get(new CBORInt(8)).getMap()
+            .set(new CBORInt(2), new CBORInt(-16));
         
         for (CBORObject key : requestMap.getKeys()) {
             if (key.getInt32() < 500) {
@@ -30,9 +104,22 @@ public class CreateSD {
                 result.set(key, value);
             }
         }
+        for (CBORObject key : requestMap.getKeys()) {
+            result.set(key, new CBORString("Signed disclosure"));
+        }
+        CBORArray unprotectedDisclosures = new CBORArray();
+        for (int i = 0; i < 4; i++) {
+            unprotectedDisclosures.add(new CBORString("Unprotected disclosure"));
+        }
+        result.set(CBORCryptoConstants.CSF_UNPROTECTED_LBL, unprotectedDisclosures);
+        byte[] cbor = new CBORAsymKeySigner(
+                issuerPair.getPrivate(), 
+                AsymSignatureAlgorithms.getAlgorithmFromId(alg))
+            .setKeyId(kid)
+            .sign(new CBORTag(OBJECT_ID, result)).encode();
+        replace("sd", createBox(CBORDecoder.decode(cbor).toString()));
 
-        System.out.println("Got here!" + result.toString());
-        System.out.println("Got here!" + requestMap.toString());
+        writeString("doc" + File.separator + "index.html", template.toString());
     }
 
 }
